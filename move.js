901d2d7771293a0df6a797c610b6576bcfdc2d53
ChimbiliSuprajaChowdary <supraja.cs1995@gmail.com>
2020-09-14 00:49:07 +0530

    done

--- move.js
+++ move.js
@@ -1,89 +1,78 @@
-var box1 = document.querySelector('#box1');
-var box2 = document.querySelector('#box2');
-var box3 = document.querySelector('#box3');
-var box4 = document.querySelector('#box4');
-var box8 = document.querySelector('#box8');
-var box12 = document.querySelector('#box12');
-var box16 = document.querySelector('#box16');
-var box15 = document.querySelector('#box15');
-var box14 = document.querySelector('#box14');
-var box13 = document.querySelector('#box13');
-var box9 = document.querySelector('#box9');
-var box5 = document.querySelector('#box5');
+var board = [
+  10,
+  20,
+  30,
+  40,
+  50,
+  60,
+  70,
+  80,
+  90,
+  100,
+  110,
+  120,
+  130,
+  140,
+  150,
+  160,
+];
+const player1_button = document.getElementById("player1");
+console.log(player1_button);
+const player2_button = document.getElementById("player2");
+console.log(player2_button);
 
+player1_button.addEventListener("click", rollDice1);
 
-var d = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
-var arrdiv = [box1, box2, box3, box4, box8, box12, box16, box15, box14, box13, box9, box5];
+var player1 = ["prograd", 0, 1000];
+var player2 = ["faceprep", 0, 1000];
 
-
-const player1 = document.getElementById('btn1');
-const player2 = document.getElementById("btn2");
-console.log(d.length)
-console.log(player1);
-console.log(player2);
-
-player1.addEventListener('click', rolldice1)
-player2.addEventListener('click', rolldice2)
-
-var person1 = ["pkp", 0, 1000];
-var person2 = ["pkp1", 0, 1000];
-
-
-function rolldice1() {
-    let position = Math.floor((Math.random() * 6) + 1);
-    console.log(" person1 play", position);
-    changeposition1(person1[1], position);
+function rollDice1() {
+  let position = Math.floor(Math.random() * 6) + 1;
+  console.log("player1 rolls dice", position);
+  changePosition1(player1[1], position);
 }
 
-function rolldice2() {
-   //fill your code
-}
-var count = 0;
-function changeposition1(old, current) {
-    count++;
-    var newposition = old + current;
-    person1[1] = newposition;
-    newposition = newposition % 11;  
-    var img = document.getElementById('img1');
-    img.style.visibility = 'hidden';
-    console.log(" count" + count);
-    if(count>1)
-     {
-         var elem = document.getElementById("img11");
-         elem.remove();
-     }
-   
-         let template = '<img src="./images/bB.png" id="img11" alt="red" style="width: 36px;">';
-         arrdiv[newposition].innerHTML += template;      
-    console.log(" person1 play", person1[1]);
-    changeMoney1(person1[1]);
-}
-var count2=0;
-function changeposition2(old, current) {
-   // type your code
+function changePosition1(old, currentPos) {
+  var newPosition = old + currentPos;
+  player1[1] = newPosition;
+  console.log(player1[1]);
+  updateMoney1(player1[1]);
 }
 
-function changeMoney1(p2) {
-    var updateMoney = 0;
-    if (p2 < d.length) {
-        updateMoney = person1[2] - d[p2 - 1];
-    } else {
-        p2 = p2 % 11;
-        updateMoney = person1[2] - d[p2 - 1];
-    }
-    person1[2] = updateMoney;
-    if (updateMoney > 0)
-    {
-    document.querySelector('.item3').innerHTML = updateMoney;
-    
-    }
-    else{
-        document.querySelector('.item3').innerHTML = "Player 2 Win";
-    }
-
+function updateMoney1(p1) {
+  var updateMoney = 0;
+  if (p1 < board.length) updateMoney = player1[2] - board[p1 - 1];
+  else {
+    p1 = p1 % 15;
+    updateMoney = player1[2] - board[p1 - 1];
+  }
+  console.log(updateMoney);
 }
 
-function changeMoney2(p2) {
-  //  type your code
+player2_button.addEventListener("click", rollDice2);
 
+var player1 = ["prograd", 0, 1000];
+var player2 = ["faceprep", 0, 1000];
+
+function rollDice2() {
+  let position = Math.floor(Math.random() * 6) + 1;
+  console.log("player2 rolls dice", position);
+  changePosition2(player2[1], position);
+}
+
+function changePosition2(old, currentPos) {
+  var newPosition = old + currentPos;
+  player2[1] = newPosition;
+  console.log(player2[1]);
+  updateMoney2(player2[1]);
+}
+
+function updateMoney2(p2) {
+  var updateMoney = 0;
+  if (p2 < board.length) updateMoney = player2[2] - board[p2 - 1];
+  else {
+    p2 = p2 % 15;
+    updateMoney = player2[2] - board[p2 - 1];
+  }
+  console.log(updateMoney);
 }
